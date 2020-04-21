355d2608b7404f6cf602ca2420838e385a8870b3
Kevin Balanta <kevin232454@hotmail.com>
2020-04-21 04:39:08 -0500

    Updates

---
+++ precache-manifest.904069ea550308cc533c863e17a9bcc0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "271b2f7363521ad10b57df5a3bbe5c56",
+    "url": "/forum/index.html"
+  },
+  {
+    "revision": "e59570de1df5968add27",
+    "url": "/forum/static/css/main.b75ac214.chunk.css"
+  },
+  {
+    "revision": "a1b914f7f03eb2331e89",
+    "url": "/forum/static/js/2.8db618b5.chunk.js"
+  },
+  {
+    "revision": "aef280276b8d6f56e9d8dc46988119d9",
+    "url": "/forum/static/js/2.8db618b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e59570de1df5968add27",
+    "url": "/forum/static/js/main.d5812091.chunk.js"
+  },
+  {
+    "revision": "446de4a4fce982e2d4d3",
+    "url": "/forum/static/js/runtime-main.f5eea032.js"
+  }
+]);
